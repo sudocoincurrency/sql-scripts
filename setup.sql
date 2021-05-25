1fbfe5119b2b64734aa2ab47da4866cd0c64655f
Roux <52894745+sudocode1@users.noreply.github.com>
2021-05-25 22:14:33 +0100

    init

---
+++ setup.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS coin;
+CREATE DATABASE IF NOT EXISTS coin;
+USE coin;
+
+CREATE TABLE users (
+    pass text NOT NULL UNIQUE,
+    coin float NOT NULL default 0,
+    id int NOT NULL UNIQUE
+);
+
+CREATE TABLE config (
+    id int default 1 NOT NULL UNIQUE,
+    coinsMined float default 0 NOT NULL,
+    coinCap float default 350 NOT NULL
+);
+
+CREATE TABLE transactions (
+    sendingUserId int NOT NULL,
+    recievingUserId int NOT NULL,
+    amountTransferred float NOT NULL,
+    transactionId int NOT NULL UNIQUE,
+    reason text NOT NULL
+);
+
+CREATE TABLE items (
+    itemName text NOT NULL,
+    amountToCharge float NOT NULL,
+    sellerId int NOT NULL,
+    stock int NOT NULL,
+    itemId int NOT NULL UNIQUE,
+    category text NOT NULL,
+    contactSeller text NOT NULL,
+    descr text NOT NULL
+);
